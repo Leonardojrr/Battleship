7c8950f0694f1b6446f81869c767608b568bd971
Leonardojrr <leonardojrr1@gmail.com>
2019-03-06 03:30:51 -0400

    Fixing bugs and validate
    fixing bugs and creating the validate function to verify the position of the ships

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 var box =  document.getElementsByClassName("box");
 var ships = [{name:"Carrier",size:5},{name:"Battleship",size:4},{name:"Cruiser",size:3},{name:"Submarine",size:3},{name:"Destroyer",size:2}]
+var board = document.getElementsByClassName("board");
+var shipObj;
+var shipsAtBoard = [];
+var rotate=false;
+var box_position=0;
+var size = ships[0].size;
+var count = 0;
+
+for(let i=0;i<=100;i++){
+    board[0].appendChild(document.createElement("div"));
+    board[0].childNodes[i].className = "box";
+}
+
 
 function putShoot(box_position){
     box_position.appendChild(document.createElement("div"));
@@ -56,137 +69,206 @@ function removeShip(size,box_position,rotate){
     }
 }
 
+function putShipsAtBoard(s){
+    if(count>0){
+        for(let i=0;i<s.length;i++){
+            putShip(s[i].shipSize,s[i].position,s[i].r);
+        }
+    }
+}
 
-        var rotate=false;
-        var choosing=true;
-        var box_position=0;
-        var size = ships[0].size;
-        var count = 1;
+function validPosition(size,box_position,rotate,s){
+    let validate = true;
+    let boxsOfShip = [];
+    let boxsOfBoardShips =[];
+
+    if(count>0){
+
+        if(rotate){
+            for(let i=0;i<size;i++){
+                boxsOfShip.push({position:box_position+i*10});
+            }
+        }
+        else{
+            for(let i=0;i<size;i++){
+                boxsOfShip.push({position:box_position+i});
+            }
+        }
+        console.log(boxsOfShip);
+
+        for(let i=0; i <s.length;i++){
+            if(s[i].r){
+                for(let x=0;x<s[i].shipSize;x++){
+                    boxsOfBoardShips.push({position:s[i].position+x*10});
+                }
+            }
+            else{
+                for(let x=0;x<s[i].shipSize;x++){
+                    boxsOfBoardShips.push({position:s[i].position+x});
+                }
+            }
+        }
+        console.log(boxsOfBoardShips);
+
+        for(var i=0;i<boxsOfShip.length;i++){
+            for(var x=0;x<boxsOfBoardShips.length;x++){
+                console.log(boxsOfShip[i].position);
+                console.log(boxsOfBoardShips[x].position);
+                if(boxsOfShip[i].position === boxsOfBoardShips[x].position){
+                    return !validate;
+                }
+            }
+        }
+        return validate;
+    }
+    else{return validate}
+}
        
         putShip(size,box_position,rotate);
 
         onkeydown = e => {
-            //DERECHA
+            if(count<5){
 
-            if(e.keyCode == 68 || e.keyCode == 39){
-                removeShip(size,box_position,rotate);
-                box_position += 1;
-                if(rotate){
-                    if(box_position%10===0){
-                        box_position = box_position - 10;
-                        putShip(size,box_position,rotate);
-                    }
-                    else{
-                        putShip(size,box_position,rotate);
-                    }
-                }
-                else{
-                    if((box_position + size - 1)%10===0){
-                        box_position = box_position-1 -(10-size);
-                        putShip(size,box_position,rotate);
-                    }
-                    else{
-                        putShip(size,box_position,rotate);
-                    }
-                }
-            }
-            //IZQUIERDA
+                //DERECHA
 
-            if(e.keyCode == 65 || e.keyCode == 37){
-                removeShip(size,box_position,rotate);
-                box_position -= 1;
-                if(rotate){
-                    if(box_position < 0 || (box_position+1)%10===0){
-                        box_position = box_position + 10;
-                        putShip(size,box_position,rotate);
+                    if(e.keyCode == 68 || e.keyCode == 39){
+                        removeShip(size,box_position,rotate);
+                        putShipsAtBoard(shipsAtBoard);
+                        box_position += 1;
+                        if(rotate){
+                            if(box_position%10===0){
+                                box_position = box_position - 10;
+                                putShip(size,box_position,rotate);
+                            }
+                            else{
+                                putShip(size,box_position,rotate);
+                            }
+                        }
+                        else{
+                            if((box_position + size - 1)%10===0){
+                                box_position = box_position-1 -(10-size);
+                                putShip(size,box_position,rotate);
+                            }
+                            else{
+                                putShip(size,box_position,rotate);
+                            }
+                        }
                     }
-                    else{
-                        putShip(size,box_position,rotate);
-                    }
-                }
-                else{
-                    if(box_position<0 || (box_position+1)%10===0 ){
-                        box_position = box_position+1+(10-size);
-                        putShip(size,box_position,rotate);
-                    }
-                    else{
-                        putShip(size,box_position,rotate);
-                    }
-                }
-            }
-            //ARRIBA
 
-            if(e.keyCode == 87 || e.keyCode == 38){
-                removeShip(size,box_position,rotate);
-                box_position -= 10;
-                if(rotate){
-                    if(box_position < 0){
-                        box_position = box_position + (110 - size*10)
-                        putShip(size,box_position,rotate);
-                    }
-                    else{
-                        putShip(size,box_position,rotate);
-                    }
-                }
-                else{
-                    if(box_position<0){
-                        box_position = box_position+100;
-                        putShip(size,box_position,rotate);
-                    }
-                    else{
-                        putShip(size,box_position,rotate);
-                    }
-                }
-            }
+                    //IZQUIERDA
 
-            //ABAJO
-
-            if(e.keyCode == 83 || e.keyCode == 40){
-                removeShip(size,box_position,rotate);
-                box_position += 10;
-                if(rotate){
-                    if( (box_position + (size-1)*10) >= 100){
-                        box_position = box_position - (110 - size*10)
-                        putShip(size,box_position,rotate);
+                    if(e.keyCode == 65 || e.keyCode == 37){
+                        removeShip(size,box_position,rotate);
+                        putShipsAtBoard(shipsAtBoard);
+                        box_position -= 1;
+                        if(rotate){
+                            if(box_position < 0 || (box_position+1)%10===0){
+                                box_position = box_position + 10;
+                                putShip(size,box_position,rotate);
+                            }
+                            else{
+                                putShip(size,box_position,rotate);
+                            }
+                        }
+                        else{
+                            if(box_position<0 || (box_position+1)%10===0 ){
+                                box_position = box_position+1+(10-size);
+                                putShip(size,box_position,rotate);
+                            }
+                            else{
+                                putShip(size,box_position,rotate);
+                            }
+                        }
                     }
-                    else{
-                        putShip(size,box_position,rotate);
-                    }
-                }
-                else{
-                    if(box_position+1>100){
-                        box_position = box_position-100;
-                        putShip(size,box_position,rotate);
-                    }
-                    else{
-                        putShip(size,box_position,rotate);
-                    }
-                }
-            }
 
-            //ROTAR
+                    //ARRIBA
 
-            if(e.keyCode == 82){
-                removeShip(size,box_position,rotate);
-                box_position = 0;
-                rotate = !rotate;
-                putShip(size,box_position,rotate);
-    
-            }
+                    if(e.keyCode == 87 || e.keyCode == 38){
+                        removeShip(size,box_position,rotate);
+                        putShipsAtBoard(shipsAtBoard);
+                        box_position -= 10;
+                        if(rotate){
+                            if(box_position < 0){
+                                box_position = box_position + (110 - size*10)
+                                putShip(size,box_position,rotate);
+                            }
+                            else{
+                                putShip(size,box_position,rotate);
+                            }
+                        }
+                        else{
+                            if(box_position<0){
+                                box_position = box_position+100;
+                                putShip(size,box_position,rotate);
+                            }
+                            else{
+                                putShip(size,box_position,rotate);
+                            }
+                        }
+                    }
 
-            //Colocar
-            if(e.keyCode == 13){
-                if(count<5){
-                    box_position = 0;
-                    size=ships[count].size;
-                    if(rotate){
+                    //ABAJO
+
+                    if(e.keyCode == 83 || e.keyCode == 40){
+                        removeShip(size,box_position,rotate);
+                        putShipsAtBoard(shipsAtBoard);
+                        box_position += 10;
+                        if(rotate){
+                            if( (box_position + (size-1)*10) >= 100){
+                                box_position = box_position - (110 - size*10)
+                                putShip(size,box_position,rotate);
+                            }
+                            else{
+                                putShip(size,box_position,rotate);
+                            }
+                        }
+                        else{
+                            if(box_position+1>100){
+                                box_position = box_position-100;
+                                putShip(size,box_position,rotate);
+                            }
+                            else{
+                                putShip(size,box_position,rotate);
+                            }
+                        }
+                    }
+
+                    //ROTAR
+
+                    if(e.keyCode == 82){
+                        removeShip(size,box_position,rotate);
+                        putShipsAtBoard(shipsAtBoard);
+                        box_position = 0;
                         rotate = !rotate;
+                        putShip(size,box_position,rotate);
+            
                     }
-                    putShip(size,box_position,rotate);
-                    count++
-                }
-                else{
-                    console.log(`Ya no hay mas barcos`);
-                }
             }
+                
+                //Colocar
+                if(e.keyCode == 13){
+                    if(count<=4){
+                        if(validPosition(ships[count].size,box_position,rotate,shipsAtBoard)){
+                            shipObj = {shipSize:ships[count].size,position:box_position,r:rotate};
+                            shipsAtBoard.push(shipObj);
+                            console.log(shipsAtBoard);
+                            count++
+                            if(count<5){
+                                box_position = 0;
+                                size=ships[count].size;
+                                if(rotate){rotate = !rotate;}
+                                putShip(size,box_position,rotate);
+                            }
+                            else{
+    
+                            }
+                        }
+                        else{
+                            console.log(`Posicion no valida`);
+                        }
+                    }
+                    else{
+                        console.log(`Ya no hay mas barcos`);
+                    }
+                }
         }
